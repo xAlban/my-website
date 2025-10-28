471fc4c818dcf0b2cf53016b8a339134b573dc11
xAlban <a.elezi68@gmail.com>
2025-10-28 16:20:13 +0100

    WIP divider

--- components/MainScreen.tsx
+++ components/MainScreen.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs'
-import { IoPersonOutline } from 'react-icons/io5'
-import { VscSettings } from 'react-icons/vsc'
-import { FaRegStar } from 'react-icons/fa'
-import { LuWrench } from 'react-icons/lu'
+import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
+import { IoPersonOutline } from "react-icons/io5";
+import { VscSettings } from "react-icons/vsc";
+import { FaRegStar } from "react-icons/fa";
+import { LuWrench } from "react-icons/lu";
 
 export default function MainScreen() {
     return (
         <div className="w-full flex flex-col md:flex-row justify-center items-center">
             <div className="w-full md:w-[50%] h-dvh snap-start p-[40px]">
-                <div className="w-full h-full flex justify-center items-center mask-[url(/image_mask.svg)] bg-[url(/profile_pic.jpg)] bg-position-[33%] bg-size-[250%] 2xl:bg-cover mask-no-repeat mask-contain mask-center">
-                </div>
+                <div className="w-full h-full flex justify-center items-center mask-[url(/image_mask.svg)] bg-[url(/profile_pic.jpg)] bg-position-[33%] bg-size-[250%] 2xl:bg-cover mask-no-repeat mask-contain mask-center"></div>
             </div>
-            <div className="w-full md:w-[50%] h-dvh text-white p-x-4 snap-start">
+            <div className="w-full md:w-[50%] h-dvh text-white p-x-4 snap-start flex relative">
+                <div className="h-full w-[30px] bg-black skew-x-[16.6deg] hidden md:block"></div>
                 <Tabs defaultValue="me" className="h-full w-full">
                     <TabsContent value="me">
                         <div className="h-full w-full flex flex-col justify-center gap-y-5 text-left">
@@ -20,7 +20,12 @@ export default function MainScreen() {
                                 TECH ENTHUSIAST
                             </h2>
                             <p className="text-xl">
-                                New mobile phone ? Framework ? I&apos;ll be up to date! I&apos;m interested in all techs at aninternational level. That&apos;s why I&apos;m open to any opportunity abroad.Hobbies: Volleyball, Photography, Competitive Video-games (link flickr or include photos here)
+                                New mobile phone ? Framework ? I&apos;ll be up
+                                to date! I&apos;m interested in all techs at
+                                aninternational level. That&apos;s why I&apos;m
+                                open to any opportunity abroad.Hobbies:
+                                Volleyball, Photography, Competitive Video-games
+                                (link flickr or include photos here)
                             </p>
                         </div>
                     </TabsContent>
@@ -32,7 +37,7 @@ export default function MainScreen() {
                             className="text-white data-[state=active]:bg-custom-green data-[state=active]:text-black"
                             value="me"
                         >
-                            <div className='flex flex-col justify-center items-center h-full w-full'>
+                            <div className="flex flex-col justify-center items-center h-full w-full">
                                 <IoPersonOutline />
                                 <div>ME</div>
                             </div>
@@ -41,7 +46,7 @@ export default function MainScreen() {
                             className="text-white data-[state=active]:bg-custom-green data-[state=active]:text-black"
                             value="skills"
                         >
-                            <div className='flex flex-col justify-center items-center h-full w-full'>
+                            <div className="flex flex-col justify-center items-center h-full w-full">
                                 <VscSettings />
                                 <div>SKILLS</div>
                             </div>
@@ -50,7 +55,7 @@ export default function MainScreen() {
                             className="text-white data-[state=active]:bg-custom-green data-[state=active]:text-black"
                             value="experiences"
                         >
-                            <div className='flex flex-col justify-center items-center h-full w-full'>
+                            <div className="flex flex-col justify-center items-center h-full w-full">
                                 <FaRegStar />
                                 <div>EXPERIENCES</div>
                             </div>
@@ -59,7 +64,7 @@ export default function MainScreen() {
                             className="text-white data-[state=active]:bg-custom-green data-[state=active]:text-black"
                             value="projects"
                         >
-                            <div className='flex flex-col justify-center items-center h-full w-full'>
+                            <div className="flex flex-col justify-center items-center h-full w-full">
                                 <LuWrench />
                                 <div>PROJECTS</div>
                             </div>
@@ -68,5 +73,5 @@ export default function MainScreen() {
                 </Tabs>
             </div>
         </div>
-    )
+    );
 }
