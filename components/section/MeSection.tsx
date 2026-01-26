fad63dd424d883ade5ba405399f3c3489aa22392
Alban <a.elezi68@gmail.com>
2026-01-26 12:48:08 +0100

    Enhance Me description

--- components/section/MeSection.tsx
+++ components/section/MeSection.tsx
@@ -23,10 +23,14 @@ export default function MeSection() {
       <div className="flex flex-col max-w-[1000px] flex-1 justify-center gap-20 md:px-[40px] py-12">
         <h2>ABOUT</h2>
         <p className="text-xl md:text-3xl">
-          New mobile phone ? Framework ? I&apos;ll be up to date!
+          Always on the lookout for the next big thing, I thrive on staying
+          ahead of the curve in our fast-evolving digital landscape. I&apos;m committed to continuous learning and technical
+          excellence.
           <br />
-          I&apos;m interested in all techs at an international level.
-          That&apos;s why I&apos;m open to any opportunity abroad.
+          <br />
+          My passion for technology is deeply rooted in its global impact, and
+          I&apos;m always ready to bring my international mindset to ambitious
+          projects and opportunities across the globe.
         </p>
         <div className="flex flex-col gap-4 text-xl md:text-3xl">
           <h3 className="font-bold text-2xl md:text-4xl">Hobbies</h3>
